b25e7a873b5ff726089686332e5571ea445d36c3
michaelcspeed <michaelcspeed@gmail.com>
2013-06-07 12:35:27 +0100

    Fixed trajectory animation

--- CraterImpact/src/com/faulkestelescope/craterimpact/ObjectTrajectoryActivity.java
+++ CraterImpact/src/com/faulkestelescope/craterimpact/ObjectTrajectoryActivity.java
@@ -5,18 +5,13 @@ import org.holoeverywhere.widget.Button;
 import org.holoeverywhere.widget.SeekBar;
 import org.holoeverywhere.widget.TextView;
 
-import control.DataProvider;
-
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Matrix;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.animation.Animation;
-import android.view.animation.LinearInterpolator;
-import android.view.animation.RotateAnimation;
-import android.widget.ImageView;
+import control.DataProvider;
 
 public class ObjectTrajectoryActivity extends Activity implements
 		OnClickListener,
@@ -25,18 +20,17 @@ public class ObjectTrajectoryActivity extends Activity implements
 	private TextView textTraj;
 	private SeekBar seekBarTraj;
 	private Button buttonNext;
-	private ImageView imageTraj;
-	private Bitmap bMap;
-	private Matrix matrix;
-	private RotateAnimation an;
+	private CustomImageView imageTraj;
 	float start = 0;
 	float finsih = 0;
+	private Bitmap bMap;
+	private static Matrix matrix;
 
 	private void findViews() {
 		textTraj = (TextView) findViewById(R.id.textProjectileAngle);
 		seekBarTraj = (SeekBar) findViewById(R.id.seekBarProjectileAngle);
 		buttonNext = (Button) findViewById(R.id.buttonNext);
-		imageTraj = (ImageView) findViewById(R.id.imageTraj);
+		imageTraj = (CustomImageView) findViewById(R.id.imageTraj);
 
 		buttonNext.setOnClickListener(this);
 		seekBarTraj.setOnSeekBarChangeListener(this);
@@ -57,21 +51,42 @@ public class ObjectTrajectoryActivity extends Activity implements
 		setContentView(R.layout.objecttrajectory);
 		findViews();
 		setTitle(R.string.lbPjAng);
+
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
-
 	};
 
 	@Override
 	public void onProgressChanged(SeekBar arg0, int progressNo, boolean arg2) {
-
-		textTraj.setText(progressNo + "\u00B0");
-
 		
+		if(progressNo == 0)
+			progressNo = 1;
 
+		imageTraj.setAngleRotation(90-progressNo);
+		
+		
+		/*
+		// Decode Image using Bitmap factory.
+		bMap = BitmapFactory.decodeResource(getResources(),
+				R.drawable.impactangle);
+
+		// Create object of new Matrix.
+		matrix = new Matrix();
+
+		// set image rotation value to 45 degrees in matrix.
+		matrix.postRotate(progressNo); // 180degrees / 60 kmph = 3
+
+		// Create bitmap with new values.
+		Bitmap bMapRotate = Bitmap.createBitmap(bMap, 0, 0, bMap.getWidth(),
+				bMap.getHeight(), matrix, true);
+
+		// put rotated image in ImageView.
+		imageTraj.setImageBitmap(bMapRotate);
+*/
+		textTraj.setText(progressNo + "\u00B0");
 	}
 
 	@Override
@@ -84,4 +99,5 @@ public class ObjectTrajectoryActivity extends Activity implements
 	public void onStopTrackingTouch(SeekBar arg0) {
 		// TODO Auto-generated method stub
 	}
+
 }
